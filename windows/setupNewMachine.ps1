87cbdba184981ed32c89476cee77936616b13d14
Ayan Sarkar <aysarkar@gmail.com>
2017-11-04 11:59:35 -0700

    - Added more programs to install
    - Updated vimrc

--- windows/setupNewMachine.ps1
+++ windows/setupNewMachine.ps1
@@ -1,5 +1,4 @@
-﻿Set-ExecutionPolicy Unrestricted
-
+﻿
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
@@ -7,9 +6,21 @@ if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
 
-choco install git -y
-choco install 7zip -y
-choco install vim -y
+$programs = @(
+	"git",
+	"7zip",
+	"vim",
+	"rust",
+	"putty",
+	"notepadplusplus"
+	"visualstudiocode"
+	"vlc"
+	"googlechrome"
+)
+
+foreach($program in $programs) {
+	Invoke-Expression "choco install $program -y"
+}
 refreshenv
 
 Write-Host "Downloading vim-plug"
@@ -22,7 +33,13 @@ $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
   )
 )
 
-git clone https://github.com/ghost7/configs $env:TEMP\configs
-cp $env:TEMP\configs\windows\_vimrc ~\
+$tempConfigPath = "$env:TEMP\configs"
+if(Test-Path $tempConfigPath) {
+    Remove-Item $tempConfigPath -Recurse -Force
+}
+
+git clone https://github.com/ghost7/configs $tempConfigPath
+cp $tempConfigPath\windows\_vimrc ~\
 
 vim +PlugInstall +qall
+
